f4e3a0a559fa17c0c47216ec5d3a72c0e1381236
Pavankumar-p-k <pavankumarunnam99.com>
2026-01-12 21:03:11 +0530

    Fix sudoku page import path - correct relative path from ../.././games/sudoku/ui to ./ui

--- web/src/app/games/sudoku/page.tsx
+++ web/src/app/games/sudoku/page.tsx
@@ -2,8 +2,7 @@
 export const dynamic = "force-dynamic";
 
 import React from "react";
-import SudokuUI from "../../games/sudoku/ui";
-
+import SudokuUI from "./ui";}
 export default function SudokuPage() {
   return (
     <main className="min-h-screen flex items-center justify-center p-4">
