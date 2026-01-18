643119fb5294d8236255d25a9845a18bd7087c68
Pavankumar-p-k <pavankumarunnam99.com>
2026-01-18 10:16:23 +0530

    Update page.tsx

--- web/src/app/games/sudoku/page.tsx
+++ web/src/app/games/sudoku/page.tsx
@@ -1,12 +1,14 @@
 "use client";
+
 export const dynamic = "force-dynamic";
 
 import React from "react";
-import SudokuUI from "../../../games/sudoku/ui"
-  ;export default function SudokuPage() {
+import SudokuGame from "../../../games/sudoku/ui";
+
+export default function SudokuPage() {
   return (
     <main className="min-h-screen flex items-center justify-center p-4">
-      <SudokuUI />
+      <SudokuGame />
     </main>
   );
 }
