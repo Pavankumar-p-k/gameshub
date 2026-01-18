6827375f4c932326daadee256e67506a44cb977a
Pavan <pavankumarunnam99@gmail.com>
2026-01-18 14:22:40 +0530

    Fix sudoku import path to correct location

--- web/src/app/games/sudoku/page.tsx
+++ web/src/app/games/sudoku/page.tsx
@@ -3,7 +3,7 @@
 export const dynamic = "force-dynamic";
 
 import React from "react";
-import { SudokuGame } from "./ui";
+import { SudokuGame } from "../../../games/sudoku/ui";
 
 export default function SudokuPage() {
   return (
